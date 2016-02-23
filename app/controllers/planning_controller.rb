538c9bee138a711432b2f9c1daf0e03956b08fdd
Matheus Meneses Campanhã Souza <matheusmmcs@sefaz.pi.gov.br>
2016-02-23 08:53:04 -0300

    Adaptações para planejamento de projetos ficar funcional
    Adaptações em querys no Arel da versão 3 do rails para a 4.

--- app/controllers/planning_controller.rb
+++ app/controllers/planning_controller.rb
@@ -67,33 +67,42 @@ class PlanningController < ApplicationController
   end
 
   def planning_by_projects
-    get_query
+    get_queryPlanning
 
     @user_synthesis = Hash.new
 
-    if @query.has_filter?('tracker_id')
-      tracker_list = Tracker.find(:all, :is_in_roadmaps, :conditions => [@query.statement_for('tracker_id').gsub('issues.tracker_id','trackers.id')], :order => 'position')
+    logger = Logger.new("/u01/redmine/redmine/log/teste.log", shift_age = 7, shift_size = 1048576)
+
+    if @queryPlanning.has_filter?('tracker_id')
+      #tracker_list = Tracker.find(:all, :is_in_roadmaps, :conditions => [@queryPlanning.statement_for('tracker_id').gsub('issues.tracker_id','trackers.id')], :order => 'position')
+      Tracker.all.order(:position)
+      #:conditions => [@queryPlanning.statement_for('tracker_id').gsub('issues.tracker_id','trackers.id')]
+      tracker_list = Tracker.all.select(:tracker_id).order(:position)
     else 
-      tracker_list = Tracker.find(:all, :is_in_roadmaps, :order => 'position')
+      tracker_list = Tracker.all.select(:tracker_id).order(:position)
     end
 
-    if @query.has_filter?('group_id')
-      projects_ids = CustomValue.select('customized_id').where(@query.sql_for_field_for('group_id', 'custom_values', 'value')).collect {|v| v.customized_id}
+    if @queryPlanning.has_filter?('group_id')
+      projects_ids = CustomValue.select('customized_id').where(@queryPlanning.sql_for_field_for('group_id', 'custom_values', 'value')).collect {|v| v.customized_id}
       project_list = Project.find(projects_ids)
     end
 
     # condition hacked from the Query model to match versions
     version_condition = '(versions.status <> \'closed\')'
-    version_condition += ' and ('+@query.statement_for('project_id').gsub('issues','versions')+' or exists (select 1 from issues where issues.fixed_version_id = versions.id and '+@query.statement_for('project_id')+'))' if @query.has_filter?('project_id')
+    version_condition += ' and ('+@queryPlanning.statement_for('project_id').gsub('issues','versions')+' or exists (select 1 from issues where issues.fixed_version_id = versions.id and '+@queryPlanning.statement_for('project_id')+'))' if @queryPlanning.has_filter?('project_id')
 
-    Version.find(:all, :conditions => [version_condition] ) \
+
+    #Version.find(:all, :conditions => [version_condition] ) \
+    Version.all.where(version_condition) \
     .select{|version| !version.completed? } \
     .each{|version|
+
       issue_condition = ''
-      issue_condition += @query.statement_for('project_id')+' and ' unless @query.statement_for('project_id').nil? 
+      issue_condition += @queryPlanning.statement_for('project_id')+' and ' unless @queryPlanning.statement_for('project_id').nil? 
+      
       # Changed condition to get only issues on version.
-      issue_condition += 'tracker_id in (?) and fixed_version_id = ? '
-      issue_condition += ' and project_id in (?) ' unless project_list.nil?
+      issue_condition += 'issues.tracker_id in (?) and fixed_version_id = ? '
+      issue_condition += ' and issues.project_id in (?) ' unless project_list.nil?
       
       #issue_condition += 'tracker_id in (?) and '+ \
       #  '( fixed_version_id = ? '+ \
@@ -108,15 +117,18 @@ class PlanningController < ApplicationController
       issue_condition << project_list unless project_list.nil?
 
       grouped_issues = Hash.new
-      Issue.visible.find(:all, :conditions => issue_condition, :include => [:status,:tracker], :order => 'project_id,tracker_id' ) \
-      .each {|issue|
+
+      logger.info { "MA OE: #{@queryPlanning.statement_for('project_id')}" }
+
+      #:order => 'project_id, tracker_id' )
+      Issue.visible.all.where(issue_condition).includes(:status,:tracker).each {|issue|
         if grouped_issues[issue.project].nil?
           grouped_issues[issue.project]=[issue]
         else
           grouped_issues[issue.project].push(issue)
         end
       }
-      
+
       grouped_issues.each{|project, issues|
         if @user_synthesis[project].nil?
           @user_synthesis[project] = Hash.new
@@ -127,6 +139,7 @@ class PlanningController < ApplicationController
           @user_synthesis[project][version].add_issues(issues)
         end
       }
+
     }
   end
   
@@ -140,20 +153,24 @@ class PlanningController < ApplicationController
     return true
   end
   
-  def get_query
-    @query = Query.new(:name => "_", :filters => {})
+  def get_queryPlanning
+
+    logger = Logger.new("/u01/redmine/redmine/log/teste.log", shift_age = 7, shift_size = 1048576)
+    
+
+    @queryPlanning = Query.new(:name => "_", :filters => {})
     user_projects = Project.visible
-    user_trackers = Tracker.find(:all, :is_in_roadmaps)
+    user_trackers = Tracker.all
 
     filters = Hash.new
-    filters['project_id'] = { :type => :list_optional, :order => 1, :values => user_projects.sort{|a,b| a.self_and_ancestors.join('/')<=>b.self_and_ancestors.join('/') }.collect{|s| [s.self_and_ancestors.join('/'), s.id.to_s] } } unless user_projects.empty?
-    filters['tracker_id'] = { :type => :list, :order => 2, :values => Tracker.find(:all, :is_in_roodmaps, :order => 'position' ).collect{|s| [s.name, s.id.to_s] } } unless user_trackers.empty?
-    filters['group_id']   = { :name => 'Grupo' ,:type => :list_optional, :order => 3, :values => ProjectCustomField.find_by_name('Grupo').possible_values}
+    filters['project_id'] = { :type => :list_optional, :values => user_projects.sort{|a,b| a.self_and_ancestors.join('/')<=>b.self_and_ancestors.join('/') }.collect{|s| [s.self_and_ancestors.join('/'), s.id.to_s] } } unless user_projects.empty?
+    filters['tracker_id'] = { :type => :list, :values => Tracker.all.order(:position).collect{|s| [s.name, s.id.to_s] } } unless user_trackers.empty?
+    filters['group_id']   = { :name => 'Grupo' ,:type => :list_optional, :values => ProjectCustomField.find_by_name('Grupo').possible_values}
 
-    @query.override_available_filters(filters)
+    @queryPlanning.override_available_filters(filters)
     if params[:f]
       build_query_from_params
     end
-    @query.filters={ 'project_id' => {:operator => "*", :values => [""]} } if @query.filters.length==0
+    @queryPlanning.filters={ 'project_id' => {:operator => "*", :values => [""]} } if @queryPlanning.filters.length==0
   end
 end
